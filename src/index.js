efe3afae333894906cab8ea35fe3b23ff9fb9f92
Audrey Costa <audreycota@nano.ufrj.br>
2022-08-19 03:22:47 -0300

    Prepare for deploy

--- src/index.js
+++ src/index.js
@@ -8,5 +8,6 @@ dotenv.config();
 const app = express();
 app.use(cors(), express.json(), router);
 
-const PORT = process.env.PORT;
-app.listen(PORT);
+app.listen(process.env.PORT, () => {
+    console.log("Server running on port " + process.env.PORT);
+});
